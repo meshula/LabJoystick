976e849aff529d54e08241bd62bb73758bc454a2
Nick Porcino <meshula@hotmail.com>
2024-08-10 11:17:31 -0700

    Add notifications for connection and disconnection of joysticks, add a menu of available controllers

--- LabJoystick/ContentView.swift
+++ LabJoystick/ContentView.swift
@@ -9,7 +9,10 @@ import GameController
 
 struct ContentView: View {
     @State private var isJoystickVisible: Bool = true
+    @State private var selectedController: GCController?
+    @State private var availableControllers: [GCController] = []
 
+    // Joystick state variables
     @State private var leftStickX: Float = 0.0
     @State private var leftStickY: Float = 0.0
     @State private var rightStickX: Float = 0.0
@@ -53,24 +56,44 @@ struct ContentView: View {
                                 .cornerRadius(4)
                         }
                         .padding(.trailing, 10)
+                        
+                        // Dropdown for selecting controller
+                        if !availableControllers.isEmpty {
+                            Menu {
+                                ForEach(availableControllers, id: \.self) { controller in
+                                    Button(action: {
+                                        selectController(controller)
+                                    }) {
+                                        Text(controller.vendorName ?? "Unknown Controller")
+                                    }
+                                }
+                            } label: {
+                                Text(selectedController?.vendorName ?? "Select Controller")
+                                    .font(.caption)
+                                    .padding(5)
+                                    .background(Color.gray)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(4)
+                            }
+                        }
                     }
                     JoystickOverlayView(
-                        leftStickX: .constant(0.0),
-                        leftStickY: .constant(0.0),
-                        rightStickX: .constant(0.0),
-                        rightStickY: .constant(0.0),
-                        leftShoulder: .constant(false),
-                        rightShoulder: .constant(false),
-                        leftTrigger: .constant(0.0),
-                        rightTrigger: .constant(0.0),
-                        buttonA: .constant(false),
-                        buttonB: .constant(false),
-                        buttonX: .constant(false),
-                        buttonY: .constant(false),
-                        menuButton: .constant(false),
-                        startButton: .constant(false),
-                        leftThumbstickButton: .constant(false),
-                        rightThumbstickButton: .constant(false)
+                        leftStickX: $leftStickX,
+                        leftStickY: $leftStickY,
+                        rightStickX: $rightStickX,
+                        rightStickY: $rightStickY,
+                        leftShoulder: $leftShoulder,
+                        rightShoulder: $rightShoulder,
+                        leftTrigger: $leftTrigger,
+                        rightTrigger: $rightTrigger,
+                        buttonA: $buttonA,
+                        buttonB: $buttonB,
+                        buttonX: $buttonX,
+                        buttonY: $buttonY,
+                        menuButton: $menuButton,
+                        startButton: $startButton,
+                        leftThumbstickButton: $leftThumbstickButton,
+                        rightThumbstickButton: $rightThumbstickButton
                     )
                     .background(Color.black.opacity(0.3))
                     Spacer()
@@ -85,7 +108,7 @@ struct ContentView: View {
                         Button(action: {
                             isJoystickVisible.toggle()
                         }) {
-                            Text("Show Joystick")
+                            Text(availableControllers.isEmpty ? "No Controllers Connected" : "Show Joystick")
                                 .font(.caption)
                                 .padding(5)
                                 .background(Color.blue)
@@ -102,29 +125,57 @@ struct ContentView: View {
     }
 
     func setupController() {
+        // Observe controller connection and disconnection
         NotificationCenter.default.addObserver(forName: .GCControllerDidConnect, object: nil, queue: .main) { _ in
-            if let controller = GCController.controllers().first {
-                controller.extendedGamepad?.valueChangedHandler = { gamepad, element in
-                    self.leftStickX = gamepad.leftThumbstick.xAxis.value
-                    self.leftStickY = gamepad.leftThumbstick.yAxis.value
-                    self.rightStickX = gamepad.rightThumbstick.xAxis.value
-                    self.rightStickY = gamepad.rightThumbstick.yAxis.value
-                    self.leftShoulder = gamepad.leftShoulder.isPressed
-                    self.rightShoulder = gamepad.rightShoulder.isPressed
-                    self.leftTrigger = gamepad.leftTrigger.value
-                    self.rightTrigger = gamepad.rightTrigger.value
-                    self.buttonA = gamepad.buttonA.isPressed
-                    self.buttonB = gamepad.buttonB.isPressed
-                    self.buttonX = gamepad.buttonX.isPressed
-                    self.buttonY = gamepad.buttonY.isPressed
-                    self.menuButton = gamepad.buttonMenu.isPressed
-                    self.startButton = ((gamepad.buttonOptions?.isPressed) != nil)
-                    self.leftThumbstickButton = gamepad.leftThumbstickButton?.isPressed ?? false
-                    self.rightThumbstickButton = gamepad.rightThumbstickButton?.isPressed ?? false
-                }
-            }
+            updateAvailableControllers()
+        }
+        
+        NotificationCenter.default.addObserver(forName: .GCControllerDidDisconnect, object: nil, queue: .main) { _ in
+            updateAvailableControllers()
+        }
+
+        // Initial setup
+        updateAvailableControllers()
+    }
+
+    func updateAvailableControllers() {
+        availableControllers = GCController.controllers()
+        if availableControllers.isEmpty {
+            selectedController = nil
+        } else if selectedController == nil {
+            selectController(availableControllers.first!)
         }
     }
+
+    func selectController(_ controller: GCController) {
+        selectedController = controller
+        setupControllerBinding(for: controller)
+    }
+
+    func setupControllerBinding(for controller: GCController) {
+        controller.extendedGamepad?.valueChangedHandler = { gamepad, element in
+            self.leftStickX = gamepad.leftThumbstick.xAxis.value
+            self.leftStickY = gamepad.leftThumbstick.yAxis.value
+            self.rightStickX = gamepad.rightThumbstick.xAxis.value
+            self.rightStickY = gamepad.rightThumbstick.yAxis.value
+            self.leftShoulder = gamepad.leftShoulder.isPressed
+            self.rightShoulder = gamepad.rightShoulder.isPressed
+            self.leftTrigger = gamepad.leftTrigger.value
+            self.rightTrigger = gamepad.rightTrigger.value
+            self.buttonA = gamepad.buttonA.isPressed
+            self.buttonB = gamepad.buttonB.isPressed
+            self.buttonX = gamepad.buttonX.isPressed
+            self.buttonY = gamepad.buttonY.isPressed
+            self.menuButton = gamepad.buttonMenu.isPressed
+            self.startButton = gamepad.buttonOptions?.isPressed ?? false
+            self.leftThumbstickButton = gamepad.leftThumbstickButton?.isPressed ?? false
+            self.rightThumbstickButton = gamepad.rightThumbstickButton?.isPressed ?? false
+        }
+    }
+}
+
+#Preview {
+    ContentView()
 }
 
 #Preview {
