24cecf87eee24dd887b0b60e7bad34d780bf22b5
Nick Porcino <meshula@hotmail.com>
2024-08-10 10:29:50 -0700

    Initial Commit

---
+++ LabJoystick/LabJoystickApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabJoystickApp.swift
+//  LabJoystick
+//
+//  Created by Domenico Porcino on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LabJoystickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
